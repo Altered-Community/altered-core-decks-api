c1b735b7d774fdcec8f72ebb19ffa24dccddc3c0
John Quairia <john@toxicity.be>
2026-05-11 14:53:15 +0200

    me route

---
+++ src/Controller/MeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MeController extends AbstractController
+{
+    public function __construct(
+        private readonly Security $security,
+    ) {}
+
+    #[Route('/api/me', name: 'api_me', methods: ['GET'])]
+    public function __invoke(): JsonResponse
+    {
+        /** @var User $user */
+        $user = $this->security->getUser();
+
+        return $this->json([
+            'email'    => $user->getEmail(),
+            'uniqueId' => $user->getKeycloakId(),
+            'nickName' => $user->getUsername(),
+        ]);
+    }
+}
