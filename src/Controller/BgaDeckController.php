3e0069635c6df8f883288aed4e7fe57dd4252fce
John Quairia <john@toxicity.be>
2026-05-21 10:40:56 +0200

    fix bga get deck

--- src/Controller/BgaDeckController.php
+++ src/Controller/BgaDeckController.php
@@ -107,7 +107,7 @@ class BgaDeckController extends AbstractController
     {
         $deck = $this->deckRepository->find($id);
 
-        if (!$deck || !$deck->getIsPublic()) {
+        if (!$deck) {
             throw new NotFoundHttpException();
         }
 
